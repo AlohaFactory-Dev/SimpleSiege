487fad37b0b272d4c4a63f4a734c29add6102872
aiden <rptdpa879@gmail.com>
2025-08-28 12:33:07 +0900

    [기능] 결과 팝업

--- Assets/_Scripts/Manager/StageManager.cs
+++ Assets/_Scripts/Manager/StageManager.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Aloha.Coconut;
 using Aloha.Coconut.UI;
 using FactorySystem;
+using UniRx;
 using UnityEngine;
 using Zenject;
 
@@ -19,14 +20,15 @@ public class StageManager : MonoBehaviour
     [Inject] private DeckSelectionManager _deckSelectionManager;
     [Inject] private CardPoolManager _cardPoolManager;
     [Inject] private StageUI _stageUI;
-    [Inject] private BuildingSpawnController _buildingSpawnController;
+    [Inject] private BuildingManager _buildingManager;
     public StageTable CurrentStageTable { get; private set; }
 
     public void Init()
     {
         CurrentStageTable = TableListContainer.Get<StageTableList>().GetStageTable(1);
         OpenPopup(StagePopupConfig.DeckSelectionViewConfig);
-        _buildingSpawnController.Init();
+        _buildingManager.Init();
+        _buildingManager.OnStageResult.Subscribe(EndStage).AddTo(this);
     }
 
     public void OpenPopup(StagePopupConfig config, UIOpenArgs args = null)
@@ -43,12 +45,12 @@ public class StageManager : MonoBehaviour
         _stageUI.Init();
     }
 
-    public void EndStage(bool isWin)
+    public void EndStage(TeamType team)
     {
         Time.timeScale = 0f;
         var args = new StageResultPopup.Args()
         {
-            IsWin = isWin
+            winner = team
         };
         _coconutCanvas.Open(nameof(StagePopupConfig.StageResultViewConfig), args);
     }
