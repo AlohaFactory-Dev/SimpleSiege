a03f91e717a283a58481e7b4dbbc76bf4e4c0e20
aiden <rptdpa879@gmail.com>
2025-09-04 19:36:10 +0900

    [기능] StageResultPOpup

--- Assets/_Scripts/UI/Stage/StageResultPopup.cs
+++ Assets/_Scripts/UI/Stage/StageResultPopup.cs
@@ -1,4 +1,5 @@
 using Aloha.Coconut.UI;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,6 +7,8 @@ public class StageResultPopup : UISlice
 {
     [SerializeField] private GameObject winUI;
     [SerializeField] private GameObject loseUI;
+    [SerializeField] private TextMeshProUGUI playTimeText;
+    [SerializeField] private TextMeshProUGUI stageNameText;
     [SerializeField] private Button restartButton;
     private bool _isInitialized = false;
 
@@ -20,6 +23,11 @@ public class StageResultPopup : UISlice
         var resultArgs = args as Args;
         if (resultArgs == null) return;
 
+        float playTime = StageConainer.Get<StageManager>().EndTime;
+        int minutes = (int)(playTime / 60);
+        int seconds = (int)(playTime % 60);
+        stageNameText.text = $"스테이지 : {StageConainer.Get<StageManager>().CurrentStageTable.stageNumber}";
+        playTimeText.text = $"경과 시간 : {minutes:D2}분 {seconds:D2}초";
         // 결과에 따라 UI 업데이트 로직 구현
         if (resultArgs.winner == TeamType.Player)
         {
