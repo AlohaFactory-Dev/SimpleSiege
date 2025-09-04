d5a0077192a901420010318a1ef0e8b9ff1f702b
aiden <rptdpa879@gmail.com>
2025-09-04 19:55:26 +0900

    빌드

--- Assets/_Scripts/Map/TutorialTextOff.cs
+++ Assets/_Scripts/Map/TutorialTextOff.cs
@@ -7,6 +7,8 @@ public class TutorialTextOff : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        gameObject.SetActive(false);
+        var player = other.GetComponent<ITarget>();
+        if (player != null && player.TeamType == TeamType.Player)
+            gameObject.SetActive(false);
     }
 }
